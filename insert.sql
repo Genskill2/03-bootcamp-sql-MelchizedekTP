262c97f7875768bcd4c9050a9dab1414d4c090dc
Mark21 <melchizedektp4@gmail.com>
2021-06-20 23:15:25 +0530

    made insert.sql

---
+++ insert.sql
@@ -0,0 +1,19 @@
+insert into publisher (name,country) values ('PHI','India');
+insert into publisher (name,country) values ('Harper','USA');
+insert into publisher (name,country) values ('GCP','USA');
+insert into publisher (name,country) values ('Avery','USA');
+insert into publisher (name,country) values ('Del Rey','UK');
+insert into publisher (name,country) values ('vintage','UK');
+
+
+insert into subjects (name) values('C');
+insert into subjects (name) values('UNIX');
+insert into subjects (name) values('Technology');
+insert into subjects (name) values('Go');
+insert into subjects (name) values('Science Fiction');
+insert into subjects (name) values('Productivity');
+insert into subjects (name) values('Psychology');
+insert into subjects (name) values('Politics');
+insert into subjects (name) values('History');
+
+insert into books (title,publisher) 
