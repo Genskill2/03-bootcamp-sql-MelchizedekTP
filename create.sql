46a6b5b93c0c337ae9cd94e8096bd3cb4be470c7
Mark21 <melchizedektp4@gmail.com>
2021-07-22 18:13:28 +0530

    changed id to integer

--- create.sql
+++ create.sql
@@ -1,21 +1,21 @@
 create table publisher (
-       id serial primary key,
+       id integer primary key,
        name text,
        country text
        );
 
 create table books (
-       id serial primary key,
+       id integer primary key,
        title text,
        publisher serial references publisher(id)
        );
 
 create table subjects(
-       id serial primary key,
+       id integer primary key,
        name text
        );
 
 create table books_subjects (
-       book serial references books(id),
+       book integer references books(id),
        subject serial references subjects(id)
        );
